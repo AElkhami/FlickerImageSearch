702043818a1c6055ee1bb63fdc6a6fc4af4e3f5a
Ahmed22 <Elkhami.it@gmail.com>
2021-07-06 21:07:17 +0200

    *Implementing view binding to all fragments.

--- app/src/main/java/com/elkhami/flickerimagesearch/view/displayimage/DisplayImageFragment.kt
+++ app/src/main/java/com/elkhami/flickerimagesearch/view/displayimage/DisplayImageFragment.kt
@@ -5,18 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.elkhami.flickerimagesearch.databinding.FragmentDisplayImageBinding
 
 /**
  * Created by A.Elkhami on 06,July,2021
  */
 class DisplayImageFragment: Fragment() {
 
+    private lateinit var binding: FragmentDisplayImageBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = FragmentDisplayImageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/elkhami/flickerimagesearch/view/savedimages/SavedImagesFragment.kt
+++ app/src/main/java/com/elkhami/flickerimagesearch/view/savedimages/SavedImagesFragment.kt
@@ -5,21 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.elkhami.flickerimagesearch.databinding.FragmentSavedImagesBinding
 
 /**
  * Created by A.Elkhami on 06,July,2021
  */
 class SavedImagesFragment: Fragment() {
 
+    private lateinit var binding: FragmentSavedImagesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = FragmentSavedImagesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
