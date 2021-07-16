dc11b52eb5894b1bbc34564b075a5a561095bda6
Ahmed22 <Elkhami.it@gmail.com>
2021-07-16 13:31:13 +0200

    *Implementing PhotoSearchViewModel.kt. *Adding Event.kt class for response status handling.

--- app/src/main/java/com/elkhami/flickerimagesearch/view/imagesearch/viewmodel/PhotoSearchViewModel.kt
+++ app/src/main/java/com/elkhami/flickerimagesearch/view/imagesearch/viewmodel/PhotoSearchViewModel.kt
@@ -1,9 +1,13 @@
 package com.elkhami.flickerimagesearch.view.imagesearch.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.elkhami.flickerimagesearch.data.local.SavedPhoto
 import com.elkhami.flickerimagesearch.data.remote.responses.FlickerPhotosResponse
 import com.elkhami.flickerimagesearch.data.repository.DefaultRepository
+import com.elkhami.flickerimagesearch.other.Event
 import com.elkhami.flickerimagesearch.other.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,10 +20,21 @@ import javax.inject.Inject
 class PhotoSearchViewModel @Inject constructor(private val repository: DefaultRepository) :
     ViewModel() {
 
-     fun searchFlickerWithKeyword(searchWord: String) {
-         viewModelScope.launch {
-             val response = repository.searchFlickerWithKeyword(searchWord)
-         }
+    private val _flickerPhotos = MutableLiveData<Event<Resource<FlickerPhotosResponse>>>()
+    var flickerPhotos: LiveData<Event<Resource<FlickerPhotosResponse>>> = _flickerPhotos
+
+
+    fun searchFlickerWithKeyword(searchWord: String) {
+        if(searchWord.isEmpty()){
+            return
+        }
+
+        _flickerPhotos.value = Event(Resource.Loading())
+
+        viewModelScope.launch {
+            val response = repository.searchFlickerWithKeyword(searchWord)
+            _flickerPhotos.value = Event(response)
+        }
     }
 
 }
